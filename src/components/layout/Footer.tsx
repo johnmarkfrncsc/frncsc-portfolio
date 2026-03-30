d8bc08a082fc8fcf01765afc2bd3870d8a77a203
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-03-30 15:00:46 +0800

    feat: add Footer component

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="px-8 py-5 border-t border-gray-200 flex justify-between items-center **:text-xs text-gray-400">
+      <p>Mark Francisco — 2025</p>
+      <p>Built with React + TypeScript</p>
+    </footer>
+  );
+};
+
+export default Footer;
