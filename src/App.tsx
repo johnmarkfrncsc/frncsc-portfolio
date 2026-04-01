0d42d6d8e5e7fc4fa853679f513f5f755646ac51
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-01 12:06:36 +0800

    feat: wire up App with all sections and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Hero from "./components/sections/Hero";
+import About from "./components/sections/About";
+import Skills from "./components/sections/Skills";
+import Project from "./components/sections/Project";
+import Contact from "./components/sections/Contact";
 function App() {
   return (
-    <main>
-      <h1>Hello Mark</h1>
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Hero />
+        <About />
+        <Skills />
+        <Project />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
