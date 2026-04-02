31fbb3e1bd92603cd33cd401992cbc01c8caefbc
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2026-04-02 19:15:20 +0800

    feat: add resume link in Hero section and update button styling

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,6 +1,7 @@
 {
   /* TODO: ADD RESUME/CV LINK IN THE HERO SECTION */
 }
+import resume from "../assets/docs/resume.pdf";
 
 const Hero = () => {
   return (
@@ -21,10 +22,23 @@ const Hero = () => {
         </p>
         <div className="flex gap-6 flex-wrap">
           <a
-            href="#projects"
-            className="text-xs font-semibold px-4 py-2 rounded-lg bg-white text-[#0d0d0d]"
+            href={resume}
+            target="_blank"
+            rel="noreferrer"
+            className="text-sm font-semibold flex gap-2 px-4 py-2 rounded-lg bg-[#0d0d0d] text-[#f9fafb] border border-[#1F2937] hover:bg-[#1F2937]"
           >
-            View projects
+            Resume
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-file-earmark-arrow-down mt-0.5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+              <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+            </svg>
           </a>
           <a
             href="https://github.com/johnmarkfrncsc"
